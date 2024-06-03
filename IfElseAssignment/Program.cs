3f9510a2920c28234ed3eaa6321a3f1279b85897
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-06-03 22:28:11 +0600

    file added

---
+++ IfElseAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace IfElseAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 90 || number > 90)
+                Console.WriteLine("A");
+            else if (number >= 80 && number <= 89)
+                Console.WriteLine("B");
+            else if (number >= 70 && number <= 79)
+                Console.WriteLine("C");
+            else if (number >= 60 && number <= 69)
+                Console.WriteLine("D");
+            else
+                Console.WriteLine("F");
+        }
+    }
+}
